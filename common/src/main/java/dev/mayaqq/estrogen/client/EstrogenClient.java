2bce7b53d2b02eabad93b383b2b814c3799662a6
sery <sery@sappho.company>
2024-10-29 20:59:06 +0100

    gender the gender change potion gender gender gender gender gender gender

--- common/src/main/java/dev/mayaqq/estrogen/client/EstrogenClient.java
+++ common/src/main/java/dev/mayaqq/estrogen/client/EstrogenClient.java
@@ -3,16 +3,21 @@ package dev.mayaqq.estrogen.client;
 import com.simibubi.create.foundation.block.connected.CTModel;
 import com.simibubi.create.foundation.block.connected.ConnectedTextureBehaviour;
 import com.tterrag.registrate.util.nullness.NonNullFunction;
+import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.client.config.ConfigSync;
 import dev.mayaqq.estrogen.client.cosmetics.EstrogenCosmetics;
 import dev.mayaqq.estrogen.client.registry.EstrogenKeybinds;
 import dev.mayaqq.estrogen.client.registry.EstrogenRenderer;
 import dev.mayaqq.estrogen.integrations.ears.EarsCompat;
+import dev.mayaqq.estrogen.registry.EstrogenAttributes;
+import dev.mayaqq.estrogen.registry.EstrogenItems;
 import dev.mayaqq.estrogen.registry.EstrogenPonderScenes;
 import earth.terrarium.botarium.util.CommonHooks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 
 public class EstrogenClient {
     public static void init() {
@@ -21,6 +26,13 @@ public class EstrogenClient {
         EstrogenRenderer.register();
         EstrogenPonderScenes.register();
         EstrogenKeybinds.register();
+
+        ItemProperties.register(EstrogenItems.GENDER_CHANGE_POTION.get(), Estrogen.id("gender"), (itemStack, clientLevel, livingEntity, i) ->{
+            Attribute boobs = EstrogenAttributes.SHOW_BOOBS.get();
+            if(livingEntity != null && livingEntity.getAttributes().hasAttribute(boobs) && livingEntity.getAttributeValue(boobs) != 0) {
+                return 1.0f;
+            } else return 0.0f;
+        });
 
         // mod compat
         if (CommonHooks.isModLoaded("ears")) {
